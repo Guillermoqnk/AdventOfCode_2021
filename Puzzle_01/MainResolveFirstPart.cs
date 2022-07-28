751fe2e755014b5783b5f9e05570b37f60db4d50
Wyllink <guillermoqnk@gmail.com>
2022-07-28 10:51:14 +0200

    First part of the first puzzle resolved

---
+++ Puzzle_01/MainResolveFirstPart.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Puzzle_01
+{
+    public class MainResolveFirstPart
+    {
+        static void Main(string[] args)
+        {
+            var fileData = new StreamReader("D:/ProyectosProgramacion/Visual/AdventOfCode/Puzzle_01/input.txt");
+            var result = fileData.ReadToEnd();
+
+            var dataArray = result.Split('\n');
+
+            string? previousData = null;
+            int increases = 0;
+
+            foreach(var d in dataArray)
+            {
+                if(previousData is null)
+                {
+                    previousData = d;
+                }
+                else
+                {
+                    if(Convert.ToInt32(d) > Convert.ToInt32(previousData) && (d != null))
+                    {
+                        increases++;
+                    }
+
+                    previousData = d;
+                }
+                if (d == null) { break; }
+            }
+
+            Console.WriteLine(increases);
+            Console.ReadLine();
+        }
+    }
+}
